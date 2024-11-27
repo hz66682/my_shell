d8753b4a2ea7490bcbe4053f3a345ab0850cf46e
hz <hz41596211@163.com>
2024-11-27 15:26:43 +0800

    "commit"

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use std::env;
 use std::io;
-use std::process::Command;
 use std::io::Write;
+use std::process::Child;
+use std::process::Command;
+use std::process::Stdio;
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     loop {
         print!("{}>", env::current_dir()?.display());
@@ -13,35 +15,52 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 eprintln!("something err");
             }
         };
-        if input.trim() == "" {
-            continue;
-        }
-        let mut args: Vec<_> = input.split_whitespace().collect();
-        let command = args[0];
-        args.remove(0);
-        match command {
-            "cd" => {
-                if args.len() > 1 {
-                    eprintln!("the number of arguments is too much");
-                    continue;
+        let mut programs = input.trim().split('|').peekable();
+        let mut flag = None;
+        while let Some(program) = programs.next() {
+            let mut args: Vec<_> = program.trim().split_whitespace().collect();
+            let command = args[0];
+            args.remove(0);
+            match command {
+                "cd" => {
+                    // pipe = Stdio::null();
+                    if args.len() > 1 {
+                        eprintln!("the number of arguments is too much");
+                        continue;
+                    }
+                    let default_path = env::var("HOME")?;
+                    if args.len() == 0 {
+                        args.push("~");
+                    }
+                    let path = args[0].replace("~", &default_path);
+                    if let Err(err) = env::set_current_dir(path) {
+                        eprintln!("{err}");
+                    }
+                    flag = None;
                 }
-                let default_path = env::var("HOME")?;
-                if args.len() == 0 {
-                    args.push("~"); 
+                "exit" => {
+                    return Ok(());
                 }
-                let path = args[0].replace("~", &default_path);
-                if let Err(err) = env::set_current_dir(path) {
-                    eprintln!("{err}");
-                }
-            }
-            "exit" => {
-                return Ok(());
-            }
-            command => {
-                if let Ok(mut child) = Command::new(command).args(args).spawn() {
-                    child.wait()?;
-                } else {
-                    eprintln!("something err");
+                command => {
+                    let cfgin = flag.map_or(Stdio::inherit(), |x: Child| Stdio::from(x.stdout.unwrap()));
+                    let child = Command::new(command)
+                        .args(args)
+                        .stdin(cfgin)
+                        .stdout(match programs.peek() {
+                            None=> {Stdio::inherit()}
+                            Some(_)=> {Stdio::piped()}
+                        })
+                        .spawn();
+                    match child {
+                        Ok(mut t) => {
+                            t.wait()?;
+                            flag = Some(t);
+                        }
+                        Err(err) => {
+                            println!("{err}");
+                            flag = None;
+                        }
+                    }
                 }
             }
         }
